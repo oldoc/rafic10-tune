3c7e53a1f9d4e09e747f596f4beaa0a76e46aa3d
oldoc <anzhulin@hotmail.com>
2019-06-22 00:11:14 +0800

    Seems work.

--- main.py
+++ main.py
@@ -19,8 +19,8 @@ def compute_precision(classnum, data):
         hit[classnum][i] =0
 
     for i in range(interval[classnum], interval[classnum+1]):
-        total += 1
         if data[i][-1]:
+            total += 1
             for j in range(8):
                 hit[classnum][j] += data[i][j]
     return hit[classnum] / total
@@ -42,6 +42,41 @@ def tune_step(classnum, netnum, data, reduce):
                 max = data[i][-5]
     return pos
 
+def compare(rafic10, cifar10):
+    sum = 0.0
+    for i in range(8):
+        sum += abs(rafic10[i] - cifar10[i])
+    return sum
+
+def disable_one_most_influential(classnum, data, cifar10_precision):
+    total = 0
+    hit = np.zeros([8])
+    for i in range(interval[classnum], interval[classnum+1]):
+        if data[i][-1]:
+            total += 1
+            for j in range(8):
+                hit[j] += data[i][j]
+
+    min = 8
+    total -= 1
+    index = -1
+    for i in range(interval[classnum], interval[classnum+1]):
+        if data[i][-1]:
+
+            precision = (hit - data[i][0:8]) / total
+
+            diff = compare(precision, cifar10_precision)
+            if diff < min:
+                min = diff
+                index = i
+    if index != -1:
+        data[index][-1] = 0
+        print(min)
+    else:
+        print("index error!")
+    return min
+
+
 with open('rafic10-result.csv','r', newline='') as file:
     file = csv.reader(file, delimiter=',')
     for row_list in file:
@@ -56,6 +91,7 @@ with open('cifar10-ratio.csv','r', newline='') as file:
         ratio.append(row_list_int)
 ratio = array(ratio)
 ratio = ratio.T
+ratio /= 100
 
 #print(data.shape[0])
 #print(ratio[0])
@@ -72,11 +108,25 @@ num[0] /= total # the rafic10 precision of class 0 on eight models
 num[1] = num[0] / ratio[0] # percentage of rafic10 on cifar10
 '''
 
+file_precision = open('final-precision.txt','w')
+file_mask = open('mask.txt', 'w')
+for num in range(10):
+    diff = 8
+    for i in range(interval[1] - interval[0]):
+        current_diff = disable_one_most_influential(num, data, ratio[num])
+        if current_diff < diff:
+            diff = current_diff
+        else:
+            break
+    pre = compute_precision(num, data)
+    for j in range(len(pre)):
+        file_precision.write("{0:1.8f},".format(pre[j]))
+    file_precision.write("\n")
 
-for i in range(10):
-    print(compute_precision(i, data))
+for i in range(interval[-1]):
+    file_mask.write("{0}\n".format(data[i][-1]))
 
-
-
+file_mask.close()
+file_precision.close()
 
 
